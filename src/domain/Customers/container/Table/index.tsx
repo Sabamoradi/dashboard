7f741f20bfba8467e6bb346cbbda1bcb5e349a64
sabamoradi <saba.moradi7719@gmail.com>
2023-10-05 13:29:05 +0330

    add action and tags in customer page

--- src/domain/Customers/container/Table/index.tsx
+++ src/domain/Customers/container/Table/index.tsx
@@ -1,7 +1,7 @@
 import { SearchOutlined } from "@ant-design/icons";
 import { useRef } from "react";
 import type { InputRef } from "antd";
-import { Input, Table } from "antd";
+import { Input, Table, Tag, Space } from "antd";
 import type { ColumnType, ColumnsType } from "antd/es/table";
 import "./style.scss";
 import { localTexts } from "../../../../locals/text";
@@ -10,6 +10,8 @@ interface DataType {
   key: string;
   name: string;
   subsDate: string;
+  tags: string[];
+  address: string;
 }
 
 type DataIndex = keyof DataType;
@@ -19,11 +21,15 @@ const data: DataType[] = [
     key: "1",
     name: "علیرضا رزاقی طاهری",
     subsDate: "1401/10/10",
+    tags: ["nice"],
+    address:'aaa'
   },
   {
     key: "2",
     name: "پارسا قدیم خانی",
     subsDate: "1401/10/10",
+    tags: ["loser"],
+    address:'bbb'
   },
 ];
 
@@ -43,6 +49,9 @@ const CustomerTable = () => {
     ),
   });
 
+  const clickItem = () => {
+    alert("hello");
+  };
   const columns: ColumnsType<DataType> = [
     {
       title: `${localTexts.customer}`,
@@ -57,6 +66,50 @@ const CustomerTable = () => {
       key: "subsDate",
       width: "15%",
       sorter: true,
+    },
+    {
+      title: "Tags",
+      key: "tags",
+      dataIndex: "tags",
+      sorter: true,
+      render: (tags: string[]) => (
+        <span>
+          {tags.map((tag) => {
+            let color = tag.length > 5 ? "geekblue" : "green";
+            if (tag === "loser") {
+              color = "volcano";
+            }
+            return (
+              <Tag color={color} key={tag}>
+                {tag.toUpperCase()}
+              </Tag>
+            );
+          })}
+        </span>
+      ),
+    },
+    {
+        title: 'Address',
+        dataIndex: 'address',
+        filters: [
+          {
+            text: 'London',
+            value: 'London',
+          },
+          {
+            text: 'New York',
+            value: 'New York',
+          },
+        ],
+      },
+    {
+      title: "Action",
+      key: "action",
+      render: (_, record) => (
+        <Space size="middle">
+          <a onClick={() => clickItem()}>جزئیات</a>
+        </Space>
+      ),
     },
   ];
 
